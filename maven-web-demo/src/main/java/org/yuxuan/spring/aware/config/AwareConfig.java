7fde05676aa2f062c3f1e06063d6c7b51ac42c45
yuxuan.han <yuxuan.han@THINKPAD-T460>
2017-12-08 14:11:34 +0800

    Spring-Aware

---
+++ maven-web-demo/src/main/java/org/yuxuan/spring/aware/config/AwareConfig.java
@@ -0,0 +1,10 @@
+package org.yuxuan.spring.aware.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("org.yuxuan.spring.aware")
+public class AwareConfig {
+
+}
