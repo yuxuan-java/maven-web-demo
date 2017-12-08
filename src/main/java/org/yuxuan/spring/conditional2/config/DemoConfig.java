f3ad50c99f5b7649fe5938c60d16ebd0ebc9a1fc
yuxuan.han <yuxuan.han@THINKPAD-T460>
2017-12-08 17:30:04 +0800

    demo fix

--- src/main/java/org/yuxuan/spring/conditional2/config/DemoConfig.java
+++ src/main/java/org/yuxuan/spring/conditional2/config/DemoConfig.java
@@ -1,6 +1,8 @@
 package org.yuxuan.spring.conditional2.config;
 
+import org.springframework.context.annotation.Bean;
 import org.yuxuan.spring.conditional2.annotation.WiselyConfiguration;
+import org.yuxuan.spring.conditional2.service.DemoService;
 /**
  * 使用组合注解
  *
@@ -8,4 +10,10 @@ import org.yuxuan.spring.conditional2.annotation.WiselyConfiguration;
  */
 @WiselyConfiguration("org.yuxuan.spring.conditional2")
 public class DemoConfig {
+	
+	@Bean
+	public DemoService demoService() {
+		return new DemoService();
+	}
+	
 }
