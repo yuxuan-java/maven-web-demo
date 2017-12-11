8cb29a4ed715cf06e76e56b589193daa5be4a682
yuxuan.han <yuxuan.han@THINKPAD-T460>
2017-12-11 16:22:17 +0800

    sprigmvc

---
+++ src/main/java/org/yuxuan/springmvc/config/MyMvcConfig.java
@@ -0,0 +1,29 @@
+package org.yuxuan.springmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("org.yuxuan.springmvc")
+public class MyMvcConfig {
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = 
+				new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/classes/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setViewClass(JstlView.class);
+		
+		return viewResolver;
+		
+	}
+	
+}
---
+++ src/main/java/org/yuxuan/springmvc/config/WebInitializer.java
@@ -0,0 +1,25 @@
+package org.yuxuan.springmvc.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = 
+				new AnnotationConfigWebApplicationContext();
+		ctx.register(MyMvcConfig.class);
+		ctx.setServletContext(servletContext);
+		//	注册Spring MVC的DispatcherServlet
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+	}
+
+}
