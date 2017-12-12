479d03146eff38666022289958e2d2f5e0a0b3e9
yuxuan.han <yuxuan.han@THINKPAD-T460>
2017-12-12 12:05:49 +0800

    删除springmvc部分代码，springmvc保存到另一项目

--- src/main/java/org/yuxuan/spring/aop/section/LogAspect.java
+++ src/main/java/org/yuxuan/spring/aop/section/LogAspect.java
@@ -1,49 +1,49 @@
-//package org.yuxuan.spring.aop.section;
-//
-//import java.lang.reflect.Method;
-//
-//import org.aspectj.lang.JoinPoint;
-//import org.aspectj.lang.annotation.After;
-//import org.aspectj.lang.annotation.Aspect;
-//import org.aspectj.lang.annotation.Before;
-//import org.aspectj.lang.annotation.Pointcut;
-//import org.aspectj.lang.reflect.MethodSignature;
-//import org.springframework.stereotype.Component;
-//import org.yuxuan.spring.aop.annotation.Action;
-//
-//@Aspect		//	��������һ������
-//@Component	//	����Ҳ��һ��Spring���������Bean
-//public class LogAspect {
-//	
-//	/**
-//	 * �����е�
-//	 */
-//	@Pointcut("@annotation(org.yuxuan.spring.aop.annotation.Action)")
-//	public void annotationPointCut(){}
-//	
-//	/**
-//	 * ����һ�����ԣ�ʹ��@Pointcut������е�
-//	 * @param joinPoint
-//	 */
-//	@After("annotationPointCut()")
-//	public void after(JoinPoint joinPoint) {
-//		//	ͨ������Ի��ע���ϵ����ԣ�Ȼ������־��صĲ���
-//		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-//		Method method = signature.getMethod();
-//		Action action = method.getAnnotation(Action.class);
-//		System.out.println("Afterע��ʽ����: " + action.name());
-//	}
-//
-//	/**
-//	 * ����һ�����ԣ�ֱ��ʹ�����ع�����Ϊ����
-//	 * @param joinPoint
-//	 */
-//	@Before("execution(* org.yuxuan.spring.aop.service.DemoMethodService.*(..))")
-//	public void before(JoinPoint joinPoint) {
-//		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
-//		Method method = signature.getMethod();
-//		//	��ȡ������
-//		System.out.println("Before��������ʽ����: " + method.getName());
-//	}
-//	
-//}
+package org.yuxuan.spring.aop.section;
+
+import java.lang.reflect.Method;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.yuxuan.spring.aop.annotation.Action;
+
+@Aspect		//	��������һ������
+@Component	//	����Ҳ��һ��Spring���������Bean
+public class LogAspect {
+	
+	/**
+	 * �����е�
+	 */
+	@Pointcut("@annotation(org.yuxuan.spring.aop.annotation.Action)")
+	public void annotationPointCut(){}
+	
+	/**
+	 * ����һ�����ԣ�ʹ��@Pointcut������е�
+	 * @param joinPoint
+	 */
+	@After("annotationPointCut()")
+	public void after(JoinPoint joinPoint) {
+		//	ͨ������Ի��ע���ϵ����ԣ�Ȼ������־��صĲ���
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		Method method = signature.getMethod();
+		Action action = method.getAnnotation(Action.class);
+		System.out.println("Afterע��ʽ����: " + action.name());
+	}
+
+	/**
+	 * ����һ�����ԣ�ֱ��ʹ�����ع�����Ϊ����
+	 * @param joinPoint
+	 */
+	@Before("execution(* org.yuxuan.spring.aop.service.DemoMethodService.*(..))")
+	public void before(JoinPoint joinPoint) {
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		Method method = signature.getMethod();
+		//	��ȡ������
+		System.out.println("Before��������ʽ����: " + method.getName());
+	}
+	
+}
--- src/main/java/org/yuxuan/spring/taskscheduled/service/TaskScheduledService.java
+++ src/main/java/org/yuxuan/spring/taskscheduled/service/TaskScheduledService.java
@@ -1,30 +1,30 @@
-//package org.yuxuan.spring.taskscheduled.service;
-//
-//import org.joda.time.DateTime;
-//import org.springframework.scheduling.annotation.Scheduled;
-//import org.springframework.stereotype.Service;
-///**
-// * ͨ��@Scheduledע�����������Ǽƻ�����
-// */
-//@Service
-//public class TaskScheduledService {
-//	
-//	private static final String pattern = "HH:mm:ss";
-//	
-//	/**
-//	 * ʹ��fixedRate����ÿ���̶�ʱ��ִ��
-//	 */
-//	@Scheduled(fixedRate = 5000)
-//	public void reportCurrentTime() {
-//		System.out.println("reportCurrentTime: " + new DateTime().toString(pattern));
-//	}
-//	
-//	/**
-//	 * ʹ��cron���Կ��԰���cron���ʽָ����ʱ��ִ��
-//	 */
-//	@Scheduled(cron = "*/10 * * * * ?")
-//	public void fixTimeExecution() {
-//		System.out.println("fixTimeExecution: " + new DateTime().toString(pattern));
-//	}
-//	
-//}
+package org.yuxuan.spring.taskscheduled.service;
+
+import org.joda.time.DateTime;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+/**
+ * ͨ��@Scheduledע�����������Ǽƻ�����
+ */
+@Service
+public class TaskScheduledService {
+	
+	private static final String pattern = "HH:mm:ss";
+	
+	/**
+	 * ʹ��fixedRate����ÿ���̶�ʱ��ִ��
+	 */
+	@Scheduled(fixedRate = 5000)
+	public void reportCurrentTime() {
+		System.out.println("reportCurrentTime: " + new DateTime().toString(pattern));
+	}
+	
+	/**
+	 * ʹ��cron���Կ��԰���cron���ʽָ����ʱ��ִ��
+	 */
+	@Scheduled(cron = "*/10 * * * * ?")
+	public void fixTimeExecution() {
+		System.out.println("fixTimeExecution: " + new DateTime().toString(pattern));
+	}
+	
+}
